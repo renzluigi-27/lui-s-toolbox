b38d4f6cd5d315b22ec08cc9a6146e753066ec19
renzluigi-27 <renzluigi@gmail.com>
2026-04-01 15:24:23 +0400

    Create india-banks.js

---
+++ india-banks.js
@@ -0,0 +1,32 @@
+const INDIA_BANKS = {
+  "HDFC0000001": {
+    bank: "HDFC Bank",
+    branch: "Mumbai Main Branch",
+    address: "HDFC Bank Ltd, Fort, Mumbai, Maharashtra 400001, India",
+    swift: "HDFCINBBXXX"
+  },
+  "SBIN0000001": {
+    bank: "State Bank of India",
+    branch: "New Delhi Main Branch",
+    address: "11 Parliament Street, New Delhi 110001, India",
+    swift: "SBININBBXXX"
+  },
+  "ICIC0000001": {
+    bank: "ICICI Bank",
+    branch: "Mumbai Main Branch",
+    address: "ICICI Bank Tower, Bandra Kurla Complex, Mumbai 400051, India",
+    swift: "ICICINBBXXX"
+  },
+  "BARB0000001": {
+    bank: "Bank of Baroda",
+    branch: "Mumbai Main Branch",
+    address: "Bank of Baroda Building, Fort, Mumbai, Maharashtra 400001, India",
+    swift: "BARBINBBXXX"
+  },
+  "PUNB0000001": {
+    bank: "Punjab National Bank",
+    branch: "New Delhi Main Branch",
+    address: "PNB House, Rajendra Place, New Delhi 110008, India",
+    swift: "PUNBINBBXXX"
+  }
+};
