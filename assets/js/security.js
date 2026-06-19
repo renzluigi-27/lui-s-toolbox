1fcfd4cd103f163510639053bfc798a336c47fd0
renzluigi-27 <renzluigi@gmail.com>
2026-06-19 09:11:11 +0400

    Update security.js

--- assets/js/security.js
+++ assets/js/security.js
@@ -1,7 +1,7 @@
 document.addEventListener('contextmenu', e => e.preventDefault());
 
 document.addEventListener('keydown', function(e) {
-  if (e.key === "F12") e.preventDefault();
+  
   if (e.ctrlKey && e.shiftKey && e.key === "I") e.preventDefault();
   if (e.ctrlKey && e.shiftKey && e.key === "J") e.preventDefault();
   if (e.ctrlKey && e.key === "u") e.preventDefault();
